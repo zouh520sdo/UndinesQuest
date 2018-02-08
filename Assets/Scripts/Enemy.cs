cbacd74ed1ccfe870160b106b70af8e51e24d5b1
zouh520sdo <zouh520sdo@gmail.com>
2018-02-07 20:29:17 -0500

    Initial enemy work

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public enum EnemyType
+    {
+        Bacteria,
+        Sediment,
+        Chemicals,
+        Nuclear
+    }
+
+    public float health;
+    public float damage;
+    public EnemyType type;
+
+    // Use for initialing before Start
+    void Awake()
+    {
+        tag = "Enemy";
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
