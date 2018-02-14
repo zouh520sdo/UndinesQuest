2b37d1dbaf884d2d314c04f504318582a7cafb4e
SimonJ-PC\SimonJ9 <simonj142857@gmail.com>
2018-02-14 13:45:35 -0500

    Title screen

---
+++ Assets/Scripts/TitleScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour {
+
+    public void Load_First_Level()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
