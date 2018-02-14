0495d59c1472c742e6b9365056552fb88d481507
zouh520sdo <zouh520sdo@gmail.com>
2018-02-14 13:55:11 -0500

    Add enemy counter text to all level

--- Assets/MainMapController.cs
+++ Assets/MainMapController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMapController : MonoBehaviour {
 
     public GameManager gameManager;
-    public GameObject canvas;
+    public EnemyManager enemyManager;
+    public GameObject image;
+    public GameObject button;
+    public Text enemyCounter;
 
     void Awake()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        enemyManager = GameObject.Find("EnemyManager").GetComponent<EnemyManager>();
         gameManager.isPaused = true;
+        enemyCounter.enabled = false;
     }
 
     // Use this for initialization
@@ -20,12 +26,14 @@ public class MainMapController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        enemyCounter.text = enemyManager.totalKilledEnemiesAmount.ToString();
+    }
 
     public void StartGame()
     {
         gameManager.isPaused = false;
-        canvas.SetActive(false);
+        enemyCounter.enabled = true;
+        image.SetActive(false);
+        button.SetActive(false);
     }
 }
