5c0efc592c6fa0f8f1dc4562ed367abde3a38d59
zouh520sdo <zouh520sdo@gmail.com>
2018-02-13 20:40:17 -0500

    add game manager and pause function

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isPaused = false;
+
+	// Use this for initialization
+	void Start () {
+        isPaused = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("p"))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+	}
+}
